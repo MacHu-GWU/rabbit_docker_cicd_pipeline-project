08f7fbe4d6896d22a0b7724409c58877d0a8dd92
Sanhe Hu <husanhe@gmail.com>
2020-03-16 22:38:37 -0400

    update create-circleci_yml

--- create_circleci_yml.py
+++ create_circleci_yml.py
@@ -1,23 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from jinja2 import Environment, Template
+from jinja2 import Template
 from pathlib_mate import Path
 from create_app import app
-from rabbit_docker_cicd_pipeline.config import RepoConfig, TagConfig
 
 HERE = Path(__file__).parent
 
 # jinja2 Template creation reference: https://jinja.palletsprojects.com/en/2.11.x/api/#jinja2.Template
-# circleci_single_template = Template(
-#     Path(HERE, ".circleci", "config-single.yml").read_text(encoding="utf-8")
-# )
-# print(Path(HERE, ".circleci", "config-parallel.yml").read_text(encoding="utf-8"))
+circleci_single_template = Template(
+    Path(HERE, ".circleci", "config-single.yml").read_text(encoding="utf-8")
+)
 circleci_parallel_template = Template(
     Path(HERE, ".circleci", "config-parallel.yml").read_text(encoding="utf-8")
 )
 
 app.plan()
-
 
 circleci_config_yml = Path(HERE, ".circleci", "config.yml")
 circleci_config_yml.write_text(
